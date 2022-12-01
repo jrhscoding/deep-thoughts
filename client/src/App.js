c981dcbe0a71648d24203659bd26b70899c7bdd6
joseph <j.hernandez.92189@gmail.com>
2022-11-30 16:12:13 -0800

    added routes to app.js via react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,16 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Home from './pages/Home';
 
@@ -23,13 +30,40 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route
+                path='/'
+                element={<Home />}
+              />
+              <Route
+                path='/login'
+                element={<Login />}
+              />
+              <Route
+                path='/signup'
+                element={<Signup />}
+              />
+              <Route
+                path='/profile'
+                element={<Profile />}
+              />
+              <Route
+                path='/thought'
+                element={<SingleThought />}
+              />
+              <Route
+              path='*'
+              element={<NoMatch />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
